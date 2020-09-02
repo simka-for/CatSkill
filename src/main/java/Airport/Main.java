f28136dae8ed7a09c76f856bb197ce928e9776ee
Simka-for <58855519+simka-for@users.noreply.github.com>
2020-09-02 16:23:06 +0300

    task №8

---
+++ src/main/java/Airport/Main.java
@@ -0,0 +1,12 @@
+package Airport;
+
+import com.skillbox.airport.Airport;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Airport airport = Airport.getInstance();
+        System.out.println(airport.getAllAircrafts().size());
+    }
+}
